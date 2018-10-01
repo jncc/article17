ca391d3deaac4bcc391dfe8f8ae66c5c88e070b8
Graham French <Graham.French@jncc.gov.uk>
2018-10-01 11:07:22 +0100

    Added recode functions to article17 package

--- R/recode.R
+++ R/recode.R
@@ -92,4 +92,386 @@ recode_population_units <- function(population_unit) {
     unlist()
 }
 
+#' Recode trends
+#' 
+#' This function changes the descriptive trends text to
+#' the abbreviated text used in the reporting tool 
+#'
+#' @param trends character, trends text
+#'
+#' @return character, abbreviated trends text
+#' @export
+#'
+#' @examples
+#' recode_trends("Increasing (+)")
+recode_trends <- function(trends) {
+  
+  tibble::as.tibble(trends) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "decreasing") ~ "D",
+             stringr::str_detect(stringr::str_to_lower(value), "increasing") ~ "I",
+             stringr::str_detect(stringr::str_to_lower(value), "stable") ~ "S",
+             stringr::str_detect(stringr::str_to_lower(value), "uncertain") ~ "U",
+             stringr::str_detect(stringr::str_to_lower(value), "unknown") ~ "Unk",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode favourable range operators
+#' 
+#' This function changes the descriptive favourable range operators 
+#' text to the abbreviated text used in the reporting tool 
+#'
+#' @param operator character, favourable range operators text
+#'
+#' @return character, abbreviated favourable range operators text
+#' @export
+#'
+#' @examples
+#' recode_favourable_range_operators("Much more than (>>)")
+recode_favourable_range_operators <- function(operator) {
+  
+  tibble::as.tibble(operator) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "approximately equal") ~ "aeq",
+             stringr::str_detect(stringr::str_to_lower(value), "less than") ~ "lt",
+             stringr::str_detect(stringr::str_to_lower(value), "^more than") ~ "mt",
+             stringr::str_detect(stringr::str_to_lower(value), "^much more than") ~ "mmt",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode reason change
+#'
+#' This function changes the descriptive reason change
+#' text to the abbreviated text used in the reporting tool 
+#'
+#' @param reason character, reason change text
+#'
+#' @return character, abbreviated freason change text
+#' @export
+#'
+#' @examples
+#' recode_reason_change("Improved knowledge/more accurate data")
+recode_reason_change <- function(reason) {
+  
+  tibble::as.tibble(reason) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "genuine") ~ "genuine",
+             stringr::str_detect(stringr::str_to_lower(value), "improved knowledge") ~ "knowledge",
+             stringr::str_detect(stringr::str_to_lower(value), "different method") ~ "method",
+             stringr::str_detect(stringr::str_to_lower(value), "^no change") ~ "noChange",
+             stringr::str_detect(stringr::str_to_lower(value), "^no information") ~ "noInfo",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode type of estimate
+#' 
+#' This function changes the descriptive type of estimate
+#' text to the abbreviated text used in the reporting tool 
+#'
+#' @param estimate character, type of estimate text
+#'
+#' @return character, type of estimate abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_type_of_estimate("Multi-year mean")
+recode_type_of_estimate <- function(estimate) {
+  
+  tibble::as.tibble(estimate) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "best estimate") ~ "estimate",
+             stringr::str_detect(stringr::str_to_lower(value), "confidence interval") ~ "interval",
+             stringr::str_detect(stringr::str_to_lower(value), "multi-year mean") ~ "mean",
+             stringr::str_detect(stringr::str_to_lower(value), "minimum") ~ "minimum",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode yes, no or unknown
+#' 
+#' This function changes the descriptive yes, no or unknown
+#' text to the abbreviated text used in the reporting tool 
+#'
+#' @param yes_no_unknown character, yes, no or unknown text
+#'
+#' @return character, yes, no or unknown abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_yes_no_unknown("Unknown")
+recode_yes_no_unknown <- function(yes_no_unknown) {
+  
+  tibble::as.tibble(yes_no_unknown) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_to_lower(value) == "no" ~ "N",
+             stringr::str_to_lower(value) == "unknown" ~ "Unk",
+             stringr::str_to_lower(value) == "yes" ~ "Y",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+  
+}
+
+#' Recode yes to x
+#'
+#' This function changes yes to x used in the reporting tool
+#' and removes no
+#'
+#' @param yes_no_x character, yes or no text
+#'
+#' @return character, x or NA_character_
+#' @export
+#'
+#' @examples
+#' recode_yes_no_x("Yes")
+recode_yes_no_x <- function(yes_no_x) {
+  
+  tibble::as.tibble(yes_no_x) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_to_lower(value) == "no" ~ NA_character_,
+             stringr::str_to_lower(value) == "yes" ~ "x",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode ranking
+#'
+#' This function changes the descriptive ranking text
+#' to the abbreviated text used in the reporting tool 
+#'
+#' @param ranking character, ranking text
+#'
+#' @return character, abbreviated ranking text
+#' @export
+#'
+#' @examples
+#' recode_ranking("High (H)")
+recode_ranking <- function(ranking) {
+  
+  tibble::as.tibble(ranking) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           ( 
+             stringr::str_detect(stringr::str_to_lower(value), "high") ~ "H",
+             stringr::str_detect(stringr::str_to_lower(value), "medium") ~ "M",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode ranking type
+#'
+#' This function changes the descriptive ranking type text
+#' to the abbreviated text used in the reporting tool 
+#' 
+#' @param type character, ranking type text
+#'
+#' @return character, ranking type abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_ranking_type("pressure_ranking")
+recode_ranking_type <- function(type) {
+  
+  tibble::as.tibble(type) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_to_lower(value) == "pressure_ranking" ~ "p",
+             stringr::str_to_lower(value) == "threat_ranking" ~ "t",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode measures identified
+#' 
+#' This function changes the descriptive measures identified text
+#' to the abbreviated text used in the reporting tool 
+#'
+#' @param measure character, measures identified text
+#'
+#' @return character, measures identified abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_measures_identified("Measures needed but cannot be identified")
+recode_measures_identified <- function(measure) {
+  
+  tibble::as.tibble(measure) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "none yet taken") ~ "ident",
+             stringr::str_detect(stringr::str_to_lower(value), "cannot be identified") ~ "notIdent",
+             stringr::str_detect(stringr::str_to_lower(value), "identified and taken") ~ "taken",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode species measures purpose
+#' 
+#' This function changes the descriptive measures purpose text
+#' for species to the abbreviated text used in the reporting tool 
+#'
+#' @param purpose character, measures purpose text 
+#'
+#' @return character, measures purpose abbreviated text 
+#' @export
+#'
+#' @examples
+#' recode_measures_purpose_species("Maintain the current range, population and/or habitat for the species")
+recode_measures_purpose_species <- function(purpose) {
+  
+  tibble::as.tibble(purpose) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "^expand") ~ "expand",
+             stringr::str_detect(stringr::str_to_lower(value), "^increase") ~ "increase",
+             stringr::str_detect(stringr::str_to_lower(value), "^maintain") ~ "maintain",
+             stringr::str_detect(stringr::str_to_lower(value), "^restore") ~ "restore",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode measures location
+#' 
+#' This function changes the descriptive measures location text
+#' to the abbreviated text used in the reporting tool 
+#'
+#' @param location character, measures location text
+#'
+#' @return character, measures location abbreviation text
+#' @export
+#'
+#' @examples
+#' recode_measures_location("Both inside and outside Natura 2000")
+recode_measures_location <- function(location) {
+  
+  tibble::as.tibble(location) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "^only inside") ~ "in",
+             stringr::str_detect(stringr::str_to_lower(value), "^both") ~ "inOut",
+             stringr::str_detect(stringr::str_to_lower(value), "^only outside") ~ "out",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode measures response
+#' 
+#' This function changes the descriptive measures response text
+#' to the abbreviated text used in the reporting tool 
+#'
+#' @param response character, measures response text
+#'
+#' @return character, measures response abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_measures_response("Medium-term results (within the next two reporting periods, 2019-2030)")
+recode_measures_response <- function(response) {
+  
+  tibble::as.tibble(response) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "^long-term") ~ "lonTerm",
+             stringr::str_detect(stringr::str_to_lower(value), "^medium-term") ~ "medTerm",
+             stringr::str_detect(stringr::str_to_lower(value), "^short-term") ~ "srtTerm",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode prospects
+#' 
+#' This function changes the descriptive prospects text
+#' to the abbreviated text used in the reporting tool 
+#'
+#' @param prospect character, prospects text
+#'
+#' @return character, prospects abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_prospects("Unknown")
+recode_prospects <- function(prospect) {
+  
+  tibble::as.tibble(prospect) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_to_lower(value) == "bad" ~ "bad",
+             stringr::str_to_lower(value) == "good" ~ "good",
+             stringr::str_to_lower(value) == "poor" ~ "poor",
+             stringr::str_to_lower(value) == "unknown" ~ "unk",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode assessments
+#' 
+#' This function changes the descriptive assessments text
+#' to the abbreviated text used in the reporting tool 
+#'
+#' @param assessments character, assessments text
+#'
+#' @return character, assessments abbreviated text
+#' @export
+#'
+#' @examples
+#' recode_assessments("Unfavourable-inadequate")
+recode_assessments <- function(assessments) {
+  
+  tibble::as.tibble(assessments) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "^favourable") ~ "FV",
+             stringr::str_detect(stringr::str_to_lower(value), "^unfavourable-inadequate") ~ "U1",
+             stringr::str_detect(stringr::str_to_lower(value), "^unfavourable-bad") ~ "U2",
+             stringr::str_detect(stringr::str_to_lower(value), "^unknown") ~ "XX",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
+
+#' Recode trends conclusion
+#' 
+#' This function changes the descriptive trends conclusion text
+#' to the abbreviated text used in the reporting tool  
+#'
+#' @param trend character, trends conclusion text
+#'
+#' @return character, trends conclusion abbreviation text
+#' @export
+#'
+#' @examples
+#' recode_trends_conclusion("Improving (+)")
+recode_trends_conclusion <- function(trend) {
+  
+  tibble::as.tibble(trend) %>% 
+    dplyr::mutate(value = dplyr::case_when
+           (
+             stringr::str_detect(stringr::str_to_lower(value), "^deteriorating") ~ "D",
+             stringr::str_detect(stringr::str_to_lower(value), "^improving") ~ "I",
+             stringr::str_detect(stringr::str_to_lower(value), "^stable") ~ "S",
+             stringr::str_detect(stringr::str_to_lower(value), "^unknown") ~ "Unk",
+             TRUE ~ value
+           )) %>% 
+    unlist()
+}
 
