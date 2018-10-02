155657320612a18b72e28aa48d779a9fd55b1bf3
Graham French <Graham.French@jncc.gov.uk>
2018-10-02 16:20:32 +0100

    Added base grid layers

--- R/mapping.R
+++ R/mapping.R
@@ -12,4 +12,29 @@
 #'   \item{Country}{multipolygon country name}
 #'   \item{UK}{multipolygon present in the UK or not (TRUE/FALSE)}
 #' }
-"base_map_oga"
+"base_map"
+
+#' Britain and Ireland TenKM grids in British National Grid
+#' 
+#' A shape file of the British and Irish 10KM grid squares
+#' in British National Grid projection (EPSG::27700)
+#' 
+#' @format A shape file with 11100 rows and 1 variables
+#' \describe{
+#'   \item{tenkm}{polygon 10KM BNG grid square label}
+#' }
+"base_grid_bng"
+
+#' Britain and Ireland look up TenKM grids in ETRS89
+#' 
+#' A shape file of the British and Irish 10KM grid squares
+#' in ETRS89 projection (EPSG::3035), matching 10KM BNG grid
+#' squares to their corresponding ETRS 10KM grid square base 
+#' on largest area of overlap.
+#' 
+#' @format A shape file with 11100 rows and 2 variables
+#' \describe{
+#'   \item{cell_code}{polygon 10KM ETRS grid square label}
+#'   \item{tenkm}{polygon 10KM BNG grid square label}
+#' }
+"base_grid_etrs"
--- data-raw/mapping_raw.R
+++ data-raw/mapping_raw.R
@@ -1,8 +1,16 @@
-# base_map_oga
+# base_map
 # The coastline boundary was derived from the Oil and Gas Authority's OGA and Lloyd's Register SNS 
 # Regional Geological Maps (Open Source) layer. Licence: Open Government Licence v3 (OGL) 
 # (http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/).Attribution: Contains 
 # data © 2017 Oil & Gas Authority.
-base_map_oga <- sf::st_read("./Range mapping/uki_boundaries/opendata/OGA/UKIrelandIoM_DECC_OSGB36.shp", stringsAsFactors = FALSE, quiet = TRUE) %>%
+base_map <- sf::st_read("./Range mapping/uki_boundaries/opendata/OGA/UKIrelandIoM_DECC_OSGB36.shp", stringsAsFactors = FALSE, quiet = TRUE) %>%
   janitor::clean_names() %>% 
   dplyr::mutate(uk = as.logical(uk))
+
+# base_grid_bng
+base_grid_bng <- sf::st_read("./Range mapping/grids/bng_10km_gbi.shp", stringsAsFactors = FALSE, quiet = TRUE)
+
+# base_grid_etrs
+base_grid_etrs <- sf::st_read("./Range mapping/grids/etrs_bng_lookup.shp", stringsAsFactors = FALSE, quiet = TRUE)
+
+
