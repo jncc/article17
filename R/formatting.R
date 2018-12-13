32f15c29945418fd3a4d682b7516798d76531c13
Graham French <Graham.French@jncc.gov.uk>
2018-12-13 09:33:40 +0000

    commenting out non-ascii characters

--- R/formatting.R
+++ R/formatting.R
@@ -113,7 +113,7 @@ format_text <- function(text) {
   # Handle unicode characters
   # https://stackoverflow.com/questions/9934856/removing-non-ascii-characters-from-data-files
   text_formatting <- text_formatting %>% 
-    dplyr::mutate(values = stringi::stri_trans_general(value, "latin-ascii"))
+    dplyr::mutate(value = stringi::stri_trans_general(value, "latin-ascii"))
   
   text_formatting <- text_formatting %>% 
     dplyr::mutate(value = stringi::stri_escape_unicode(value),
@@ -142,23 +142,23 @@ format_text <- function(text) {
     dplyr::mutate(value = stringr::str_replace_all(value, "\\\\U00100202", "'"), # apostrophe
                   value = stringr::str_replace_all(value, "’", "'"), # apostrophe
                   value = stringr::str_replace_all(value, "00a0", ""), # No-break space
-                  value = stringr::str_replace_all(value, "00f6", "o"), # ö
-                  value = stringr::str_replace_all(value, "00d8", "O"), # Ø
-                  value = stringr::str_replace_all(value, "00f1", "n"), # ñ
-                  value = stringr::str_replace_all(value, "00c0", "A"), # À
-                  value = stringr::str_replace_all(value, "00e9", "e"), # é
-                  value = stringr::str_replace_all(value, "00e1", "a"), # á
-                  value = stringr::str_replace_all(value, "00fc", "u"), # ü
-                  value = stringr::str_replace_all(value, "015d", "s"), # ŝ
-                  value = stringr::str_replace_all(value, "u0161", "us"), # Juŝkaitis
-                  value = stringr::str_replace_all(value, "016b", "u"), # ū
-                  value = stringr::str_replace_all(value, "t0117", "te"), # Kitrytė
-                  value = stringr::str_replace_all(value, "010d", "c"), # č
-                  value = stringr::str_replace_all(value, "u0159", "ur"), # Juřičková
-                  value = stringr::str_replace_all(value, "00e4", "a"), # ä
-                  value = stringr::str_replace_all(value, "00d6", "O"), # Ö
-                  value = stringr::str_replace_all(value, "00e7", "c"), # ç
-                  value = stringr::str_replace_all(value, "00e3", "a"), # ã
+                  #value = stringr::str_replace_all(value, "00f6", "o"), # ö
+                  #value = stringr::str_replace_all(value, "00d8", "O"), # Ø
+                  #value = stringr::str_replace_all(value, "00f1", "n"), # ñ
+                  #value = stringr::str_replace_all(value, "00c0", "A"), # À
+                  #value = stringr::str_replace_all(value, "00e9", "e"), # é
+                  #value = stringr::str_replace_all(value, "00e1", "a"), # á
+                  #value = stringr::str_replace_all(value, "00fc", "u"), # ü
+                  #value = stringr::str_replace_all(value, "015d", "s"), # ŝ
+                  #value = stringr::str_replace_all(value, "u0161", "us"), # Juŝkaitis
+                  #value = stringr::str_replace_all(value, "016b", "u"), # ū
+                  #value = stringr::str_replace_all(value, "t0117", "te"), # Kitrytė
+                  #value = stringr::str_replace_all(value, "010d", "c"), # č
+                  #value = stringr::str_replace_all(value, "u0159", "ur"), # Juřičková
+                  #value = stringr::str_replace_all(value, "00e4", "a"), # ä
+                  #value = stringr::str_replace_all(value, "00d6", "O"), # Ö
+                  #value = stringr::str_replace_all(value, "00e7", "c"), # ç
+                  #value = stringr::str_replace_all(value, "00e3", "a"), # ã
                   value = stringr::str_replace_all(value, "\\\\'", "'"))
   
   # Remove trailing white space
